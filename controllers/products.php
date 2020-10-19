2980be1e6a0ecdbf8101e69341168d893266af99
Stella Dare <stelladare2@gmail.com>
2020-10-19 16:57:34 +0000

    Refactor products API to be search,category,price,sortBy filter combination

--- controllers/products.php
+++ controllers/products.php
@@ -5,10 +5,11 @@ $sort_By =  array_key_exists('sortBy',$_GET)? $_GET['sortBy'] : 'id';
 $category_name = array_key_exists("category_name",$_GET)? $_GET['category_name'] : 'category_name';
 $lPrice = array_key_exists("lPrice",$_GET)? $_GET['lPrice'] : 'price';
 $uPrice = array_key_exists("uPrice",$_GET)? $_GET['uPrice'] : 'price';
+$search_term = array_key_exists('search',$_GET)? $_GET['search'] : 'name';
 
 $table = array_key_exists('sortBy',$_GET)? $_GET['sortBy']==='best'? 'orders' : 'products' : 'products';
 $join_products_orders = $table === "orders"? "INNER JOIN products ON orders.product_id=products.id" : "";
-$where_clause = "WHERE category_name = $category_name AND price >= $lPrice AND price <= $uPrice";
+$where_clause = "WHERE category_name = $category_name AND name LIKE $search_term AND price >= $lPrice AND price <= $uPrice";
 $order_bestseling = $table === "orders"? "GROUP BY product_id ORDER BY COUNT(*) DESC" : "";
 $order_all_or_new = $table === "products"? $sort_By === 'id'? "ORDER BY $sort_By ASC" : "ORDER BY $sort_By DESC" :"";
 
