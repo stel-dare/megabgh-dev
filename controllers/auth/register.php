b828965d2ed20604c52c32f3d205a84bf95524a1
Stella Dare <stelladare2@gmail.com>
2020-11-16 11:09:53 +0000

    User Registration; Start session and set session variables when user is successfully registerd

--- controllers/auth/register.php
+++ controllers/auth/register.php
@@ -1,3 +1,6 @@
 <?php
+session_start();
+?>
+<?php
 $username_or_email_taken = isset($_GET['taken'])? $_GET['taken'] : '';
 require "views/auth/register.view.php";
--- controllers/auth/register_user.php
+++ controllers/auth/register_user.php
@@ -15,5 +15,10 @@ else{
     $hashed_password = password_hash($_POST['password'],PASSWORD_DEFAULT);
     $app['queryBuilder']->addUser($_POST['email'],$_POST['username'],$_POST['fName'],$_POST['lName'],$hashed_password);
     // echo "User added!";
+    $user = $app['queryBuilder']->fetchUser('email',$_POST['email'])[0];
+    session_start();
+        $_SESSION['logged_in'] = true;
+        $_SESSION['id'] = $user->id;
+        $_SESSION['username'] = $user->username;
     header("Location: shop");
 }
