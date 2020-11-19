7b32b481eee318662e36cc3418b120da2e07a482
Stella Dare <stelladare2@gmail.com>
2020-11-19 17:52:39 +0000

    User Account Page: Fetch User Addresses

--- controllers/user_account/user_account.php
+++ controllers/user_account/user_account.php
@@ -8,7 +8,10 @@ if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']===true){
     $user_payment_methods = $app['queryBuilder']->theExecutioner(
     "SELECT customer_id,payment_id,phone_number,name,image_url FROM `customer_payment_methods` INNER JOIN payment_methods on customer_payment_methods.payment_id = payment_methods.id WHERE customer_id=$user->id
     ");
-    // die(var_dump($user_payment_methods));
+    $user_address = $app['queryBuilder']->theExecutioner("
+    SELECT * FROM `customer_address` WHERE customer_id = $user->id;
+    ");
+    // die(var_dump($user_address));
     require "views/user_account/user_account.view.php";
     
 }
